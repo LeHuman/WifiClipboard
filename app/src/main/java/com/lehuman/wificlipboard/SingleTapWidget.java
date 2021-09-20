5410c97e2df48e6577771f2e78eceb5450e6a38c
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-09-19 22:39:44 -0500

    Tint remove view

--- app/src/main/java/com/lehuman/wificlipboard/SingleTapWidget.java
+++ app/src/main/java/com/lehuman/wificlipboard/SingleTapWidget.java
@@ -8,24 +8,39 @@ import android.content.ClipboardManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
+import androidx.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class SingleTapWidget extends AppWidgetProvider {
+
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({READY, ERROR, WAITING, STANDBY, HIDDEN})
+    @interface StatusColor {
+    }
+
+    private static final int READY = 0;
+    private static final int ERROR = 1;
+    private static final int WAITING = 2;
+    private static final int STANDBY = 3;
+    private static final int HIDDEN = 4;
+    private static final int[] colors = {0, 0, 0, 0, 0};
 
     public static final String TCP_RECEIVE = "com.lehuman.wificlipboard.TCP_RECEIVE";
     public static final String TCP_DONE = "com.lehuman.wificlipboard.TCP_DONE";
-    public static final String TCP_STATUS = "com.lehuman.wificlipboard.TCP_STATUS";
+    private static final StatusCoordinator statusCoordinator = new StatusCoordinator();
 
     public static int TCP_PORT = Settings.TCP_DEFAULT_PORT;
     public static int TCP_TIMEOUT = Settings.TCP_DEFAULT_TIMEOUT;
     public static boolean TCP_ENABLE_TOAST = Settings.TCP_DEFAULT_TOAST;
 
-    private static Handler delayedIntent;
     private static TCPServer server;
     private static RemoteViews mWidgetView;
     private static int status = -1;
@@ -45,61 +60,92 @@ public class SingleTapWidget extends AppWidgetProvider {
         newServer(context);
     }
 
-    public enum IconState {
-        READY,
-        WAITING,
-        ERROR,
-        STANDBY,
-        HIDDEN
-    }
+    private static class StatusCoordinator extends Thread {
 
-    private static void clearStandbyQueue() {
-        if (Build.VERSION.SDK_INT < 28)
-            delayedIntent.removeCallbacksAndMessages(null);
-        else
-            delayedIntent.removeCallbacksAndMessages(1);
-    }
+        private static class Status {
+            final Context context;
+            @StatusColor
+            final int color;
+            final int delay;
+            Status nextStatus;
 
-    private static void queueStandby(Context context) {
-        clearStandbyQueue();
-        if (Build.VERSION.SDK_INT < 28) {
-            delayedIntent.postDelayed(() -> {
-                Intent intent = new Intent(context, SingleTapWidget.class);
-                intent.putExtra("STATE", IconState.STANDBY);
-                intent.setAction(TCP_STATUS);
-                context.sendBroadcast(intent);
-            }, 1500);
-        } else {
-            delayedIntent.postDelayed(() -> {
-                Intent intent = new Intent(context, SingleTapWidget.class);
-                intent.putExtra("STATE", IconState.STANDBY);
-                intent.setAction(TCP_STATUS);
-                context.sendBroadcast(intent);
-            }, 1, 1500);
+            public Status(Context context, @StatusColor int color, int delay) {
+                this.context = context;
+                this.color = color;
+                this.delay = delay;
+            }
+
+            public Status(Context context, @StatusColor int color, int delay, Status nextStatus) {
+                this(context, color, delay);
+                this.nextStatus = nextStatus;
+            }
         }
-    }
 
-    private static void blink(Context context, IconState active, IconState blinked) { // TODO: better way to blink, then remove SDK conditional
-        delayedIntent.removeCallbacksAndMessages(null);
-        setIconState(context, blinked);
-        delayedIntent.postDelayed(() -> {
-            Intent intent = new Intent(context, SingleTapWidget.class);
-            intent.putExtra("STATE", active);
-            intent.setAction(TCP_STATUS);
-            context.sendBroadcast(intent);
-        }, 100);
-        delayedIntent.postDelayed(() -> {
-            Intent intent = new Intent(context, SingleTapWidget.class);
-            intent.putExtra("STATE", blinked);
-            intent.setAction(TCP_STATUS);
-            context.sendBroadcast(intent);
-        }, 200);
-        delayedIntent.postDelayed(() -> {
-            Intent intent = new Intent(context, SingleTapWidget.class);
-            intent.putExtra("STATE", active);
-            intent.setAction(TCP_STATUS);
-            context.sendBroadcast(intent);
-        }, 300);
+        private static final AtomicBoolean running = new AtomicBoolean();
+        private static final Object lock = new Object();
+        private Status nextStatus;
+
+        @Override
+        public void run() {
+            synchronized (lock) {
+                while (true) {
+                    int lastColor = -1;
+                    Context context = null;
+                    try {
+                        if (nextStatus == null)
+                            lock.wait();
+                        running.set(true);
+                        do {
+                            context = nextStatus.context;
+                            if (nextStatus.delay != 0)
+                                lock.wait(nextStatus.delay);
+                            lastColor = nextStatus.color;
+                            mWidgetView.setInt(R.id.ConnectionStatus, "setColorFilter", colors[lastColor]);
+                            updateWidgets(context);
+                        } while ((nextStatus = nextStatus.nextStatus) != null);
+                    } catch (InterruptedException ignored) {
+                    } finally {
+                        running.set(false);
+                    }
+                    if (context != null && (lastColor == ERROR || lastColor == READY))
+                        setColor(context, STANDBY, 1000);
+                }
+            }
+        }
+
+        public void update() {
+            if (!running.get())
+                synchronized (lock) {
+                    lock.notify();
+                }
+        }
+
+        public void cancel() {
+            if (running.get()) {
+                interrupt();
+            }
+        }
+
+        public void setColor(Context context, @StatusColor int color) {
+            setColor(context, color, 0);
+        }
+
+        public void setColor(Context context, @StatusColor int color, int delay) {
+            cancel();
+            nextStatus = new Status(context, color, delay);
+            update();
+        }
+
+        public void blink(Context context, @StatusColor int onColor, @StatusColor int offColor, int delay, int count) {
+            cancel();
+            count *= 2;
+            nextStatus = new Status(context, onColor, delay);
+            while (count > 0) {
+                nextStatus = new Status(context, count % 2 == 1 ? onColor : offColor, delay, nextStatus);
+                count--;
+            }
+            update();
+        }
     }
 
     private static void updateWidgets(Context context) {
@@ -112,64 +158,21 @@ public class SingleTapWidget extends AppWidgetProvider {
         context.sendBroadcast(update);
     }
 
-    private static void setIconState(Context context, IconState state) {
-        if (mWidgetView == null)
-            getWidgetView(context);
-
-        switch (state) {
-            case READY:
-                mWidgetView.setViewVisibility(R.id.ConnectionReady, View.VISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionStandby, View.INVISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionWaiting, View.INVISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionError, View.INVISIBLE);
-                queueStandby(context);
-                break;
-            case WAITING:
-                mWidgetView.setViewVisibility(R.id.ConnectionWaiting, View.VISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionStandby, View.INVISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionReady, View.INVISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionError, View.INVISIBLE);
-                clearStandbyQueue();
-                break;
-            case ERROR:
-                mWidgetView.setViewVisibility(R.id.ConnectionError, View.VISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionStandby, View.INVISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionReady, View.INVISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionWaiting, View.INVISIBLE);
-                queueStandby(context);
-                break;
-            case HIDDEN:
-                mWidgetView.setViewVisibility(R.id.ConnectionError, View.INVISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionStandby, View.INVISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionReady, View.INVISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionWaiting, View.INVISIBLE);
-                break;
-            case STANDBY:
-            default:
-                mWidgetView.setViewVisibility(R.id.ConnectionStandby, View.VISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionReady, View.INVISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionWaiting, View.INVISIBLE);
-                mWidgetView.setViewVisibility(R.id.ConnectionError, View.INVISIBLE);
-                break;
-        }
-        updateWidgets(context);
-    }
-
     private static void printStatus(Context context) {
         switch (status) {
             case 1:
                 showToast(context, "Invalid response");
-                setIconState(context, IconState.ERROR);
+                statusCoordinator.setColor(context, ERROR);
                 break;
             case -1:
                 showToast(context, "Timeout / Disconnected");
-                setIconState(context, IconState.ERROR);
-                blink(context, IconState.ERROR, IconState.HIDDEN);
+                statusCoordinator.setColor(context, ERROR);
+                statusCoordinator.blink(context, ERROR, HIDDEN, 50, 4);
                 break;
             default:
                 showToast(context, "Response copied to clipboard");
-                setIconState(context, IconState.READY);
-                blink(context, IconState.READY, IconState.STANDBY);
+                statusCoordinator.setColor(context, READY);
+                statusCoordinator.blink(context, READY, STANDBY, 100, 2);
                 break;
         }
         status = -1;
@@ -182,7 +185,6 @@ public class SingleTapWidget extends AppWidgetProvider {
 
     private static void newServer(Context context) {
         server = new TCPServer(TCP_PORT);
-        delayedIntent = new Handler(context.getMainLooper());
         server.setListener(message -> {
             if (message == null) {
                 status = -1;
@@ -209,17 +211,16 @@ public class SingleTapWidget extends AppWidgetProvider {
             count++;
             if (count > 1) {
                 server.stop();
-                delayedIntent.removeCallbacksAndMessages(null);
-                setIconState(context, IconState.ERROR);
+                statusCoordinator.setColor(context, ERROR);
                 showToast(context, "Canceling");
                 return;
             }
             showToast(context, "Already Running");
-            blink(context, IconState.WAITING, IconState.ERROR);
+            statusCoordinator.blink(context, WAITING, ERROR, 100, 2);
             return;
         }
         count = 0;
-        setIconState(context, IconState.WAITING);
+        statusCoordinator.setColor(context, WAITING);
         server.start();
         server.setTimeout(context, TCP_TIMEOUT);
         server.receive();
@@ -237,9 +238,6 @@ public class SingleTapWidget extends AppWidgetProvider {
             case TCP_DONE:
                 printStatus(context);
                 break;
-            case TCP_STATUS:
-                setIconState(context, (IconState) intent.getSerializableExtra("STATE"));
-                break;
         }
     }
 
@@ -251,10 +249,13 @@ public class SingleTapWidget extends AppWidgetProvider {
         mWidgetView = new RemoteViews(context.getPackageName(), R.layout.single_tap_widget_layout);
         mWidgetView.setOnClickPendingIntent(R.id.mainIcon, pendingIntent);
         mWidgetView.setImageViewResource(R.id.mainIcon, R.drawable.round_content_copy_24);
-        mWidgetView.setImageViewResource(R.id.ConnectionStandby, R.drawable.ic_round_wifi_24_mod); // TODO: find way to change tint instead
-        mWidgetView.setImageViewResource(R.id.ConnectionReady, R.drawable.ic_round_wifi_24_mod);
-        mWidgetView.setImageViewResource(R.id.ConnectionWaiting, R.drawable.ic_round_wifi_24_mod);
-        mWidgetView.setImageViewResource(R.id.ConnectionError, R.drawable.ic_round_wifi_24_mod);
+        mWidgetView.setImageViewResource(R.id.ConnectionStatus, R.drawable.ic_round_wifi_24_mod);
+
+        colors[READY] = context.getColor(R.color.green);
+        colors[ERROR] = context.getColor(R.color.red);
+        colors[WAITING] = context.getColor(R.color.yellow);
+        colors[STANDBY] = context.getColor(R.color.blue);
+        colors[HIDDEN] = Color.TRANSPARENT;
 
         loadSettings(context);
         if (server == null) {
@@ -271,9 +272,14 @@ public class SingleTapWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        if (appWidgetIds.length < 1)
+        final int N = appWidgetIds.length;
+        if (N < 1)
             return;
-        appWidgetManager.updateAppWidget(appWidgetIds, getWidgetView(context));
+        if (!statusCoordinator.isAlive())
+            statusCoordinator.start();
+
+        RemoteViews views = getWidgetView(context);
+        appWidgetManager.updateAppWidget(appWidgetIds, views);
     }
 
     @Override
@@ -284,8 +290,10 @@ public class SingleTapWidget extends AppWidgetProvider {
 
     @Override
     public void onEnabled(Context context) { // TODO: Fix slow start on reboot
+        if (!statusCoordinator.isAlive())
+            statusCoordinator.start();
         newWidgetView(context);
         updateWidgets(context);
-        setIconState(context, IconState.ERROR);
+        statusCoordinator.setColor(context, ERROR);
     }
 }
